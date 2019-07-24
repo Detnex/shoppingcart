3ec44b71bb67af0b1f4e3fffa9563b7ab07cf24b
Detnex <manjeetit3026@gmail.com>
2019-07-24 21:09:17 +0530

    Updates

---
+++ precache-manifest.cc25306667376f67eba3e41309bf8de9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c159162d3a808ddc9c0a9dfd23a300",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "0ef22a84b69c2b41da53",
+    "url": "/shoppingcart/static/css/main.86e58767.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/shoppingcart/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "0ef22a84b69c2b41da53",
+    "url": "/shoppingcart/static/js/main.a8d19449.chunk.js"
+  },
+  {
+    "revision": "1a5c43a575972097abbc",
+    "url": "/shoppingcart/static/js/runtime~main.a3ba74b3.js"
+  }
+]);
